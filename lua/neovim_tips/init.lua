75311e0de0844b4e7a2bef391d7a8eec20f7953b
Sasa Markovic <provincijalac@gmail.com>
2025-08-09 23:32:31 +0200

    Implement tips auto-reload on user file update

--- lua/neovim_tips/init.lua
+++ lua/neovim_tips/init.lua
@@ -3,6 +3,9 @@ local config = require("neovim_tips.config")
 local loader = require("neovim_tips.loader")
 local ui = require("neovim_tips.ui")
 
+local tmp_file = config.options.tmp_file
+local user_file = config.options.user_file
+
 function M.setup(opts)
   config.setup(opts)
   loader.load()
@@ -10,11 +13,10 @@ function M.setup(opts)
   vim.api.nvim_create_user_command("NeovimTips", ui.show_fzf, {})
 
   vim.api.nvim_create_user_command("NeovimTipsEdit", function()
-    vim.cmd("edit " .. config.options.user_file)
+    vim.cmd("edit " .. user_file)
   end, {})
 
   vim.api.nvim_create_user_command("NeovimTipsAdd", function()
-    local path = config.options.user_file
     local lines = {
       "# Title: ",
       "# Category: ",
@@ -28,17 +30,28 @@ function M.setup(opts)
       "===",
       ""
     }
-    vim.cmd("edit " .. path)
+    vim.cmd("edit " .. user_file)
     vim.api.nvim_buf_set_lines(0, -1, -1, false, lines)
     vim.cmd("normal G")
   end, {})
 
   vim.api.nvim_create_autocmd("VimLeave", {
     callback = function()
-      -- you can safely ignore possible errors
-      os.remove(config.options.tmp_file)
+      local fd = io.open(tmp_file, "r")
+      if fd then
+        fd:close()
+        os.remove(tmp_file)
+      end
     end,
   })
+
+  vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = user_file,
+    callback = function(_)
+      loader.load()
+    end,
+  })
+
 end
 
 return M
