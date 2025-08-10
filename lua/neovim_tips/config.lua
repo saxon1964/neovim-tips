5980cec7bf8f78da523ed3ca1c9bb6c65d71957f
Sasa Markovic <provincijalac@gmail.com>
2025-08-10 08:28:55 +0200

    Change extension of tip files to .md
    Change extension of user defined tips to .md

--- lua/neovim_tips/config.lua
+++ lua/neovim_tips/config.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.options = {
-  user_file = vim.fn.stdpath("config") .. "/neovim_tips/user_tips.txt",
+  user_file = vim.fn.stdpath("config") .. "/neovim_tips/user_tips.md",
   md_preview_cmd = "CLICOLOR_FORCE=1 COLORTERM=truecolor glow %s",
   tmp_file = vim.fn.expand("~/neovim_tips_tmp.md")
 }
